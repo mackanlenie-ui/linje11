8a62d8fb595aa80f069cf6e76c150407d4258946
mackanlenie-ui <mackanlenie@gmail.com>
2026-09-15 11:03:17 +0200

    Visa korrekt 1.8-version i appmenyn

---
+++ app/src/main/java/se/minaarbetspass/MainActivityCurrent.java
@@ -0,0 +1,26 @@
+package se.minaarbetspass;
+
+import android.app.AlertDialog;
+import android.view.View;
+import android.widget.PopupMenu;
+
+public class MainActivityCurrent extends MainActivityV18 {
+    @Override void showMenu(View anchor){
+        PopupMenu p=new PopupMenu(this,anchor);
+        p.getMenu().add("Säkerhetskopiera");
+        p.getMenu().add("Återställ säkerhetskopia");
+        p.getMenu().add("Passmallar");
+        p.getMenu().add("Påminnelser");
+        p.getMenu().add("Om appen");
+        p.setOnMenuItemClickListener(i->{
+            String s=i.getTitle().toString();
+            if(s.startsWith("Säker"))exportData();
+            else if(s.startsWith("Åter"))importData();
+            else if(s.equals("Passmallar"))chooseTemplate();
+            else if(s.equals("Påminnelser"))reminderSettings();
+            else new AlertDialog.Builder(this).setTitle("Mina arbetspass").setMessage("Version 1.8\n\nDina uppgifter sparas endast lokalt i telefonen.\n\nLenies schema används som separat översikt och får inga arbetspåminnelser.").setPositiveButton("OK",null).show();
+            return true;
+        });
+        p.show();
+    }
+}
